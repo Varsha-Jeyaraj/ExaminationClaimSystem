4b61753416286815e1aa559aa662ad7a2a763528
Nimshan Tharamasinghe <153011035+NimshanDilunika@users.noreply.github.com>
2024-10-31 11:39:17 +0530

    Update userRegister.php
    diwaliday

--- userRegister.php
+++ userRegister.php
@@ -9,14 +9,9 @@ $username = $_POST['username'] ?? '';
 $password = $_POST['password'] ?? '';
 $usertype = $_POST['usertype'] ?? '';
 
-// Debug output
-echo "<pre>";
-var_dump($name, $nic, $designation, $username, $password, $usertype);
-echo "</pre>";
-
 // Check for empty fields
 if (empty($name) || empty($nic) || empty($designation) || empty($username) || empty($password) || empty($usertype)) {
-    echo 'All fields are required!';
+    header('Location: registerform.php?error=' . urlencode('All fields are required!'));
     exit;
 }
 
@@ -29,8 +24,8 @@ $checkStmt->bind_result($count);
 $checkStmt->fetch();
 
 if ($count > 0) {
-    echo "Username already exists!";
     $checkStmt->close();
+    header('Location: registerform.php?error=' . urlencode('  User registered not successfully!  Username already exists!'));
     exit;
 }
 $checkStmt->close();
@@ -40,15 +35,15 @@ $sql1 = "INSERT INTO userdetails (username, name, nic, designation) VALUES (?, ?
 $statement1 = $conn->prepare($sql1);
 
 if (!$statement1) {
-    die("Statement preparation failed: " . $conn->error);
+    header('Location: registerform.php?error=' . urlencode("Statement preparation failed: " . $conn->error));
+    exit;
 }
 
 $statement1->bind_param("ssss", $username, $name, $nic, $designation);
 
-if ($statement1->execute()) {
-    echo 'Insert Data user details Success!';
-} else {
-    die("Insert Data Error: " . $statement1->error);
+if (!$statement1->execute()) {
+    header('Location: registerform.php?error=' . urlencode("Insert Data Error: " . $statement1->error));
+    exit;
 }
 
 // Prepare and execute the second SQL query
@@ -56,18 +51,22 @@ $sql2 = "INSERT INTO users (username, password, usertype) VALUES (?, ?, ?)";
 $statement2 = $conn->prepare($sql2);
 
 if (!$statement2) {
-    die("Statement preparation failed: " . $conn->error);
+    header('Location: registerform.php?error=' . urlencode("Statement preparation failed: " . $conn->error));
+    exit;
 }
 
 $statement2->bind_param("sss", $username, $password, $usertype);
 
-if ($statement2->execute()) {
-    echo 'Insert Data user Success!';
-} else {
-    die("Insert Data Error: " . $statement2->error);
+if (!$statement2->execute()) {
+    header('Location: registerform.php?error=' . urlencode("Insert Data Error: " . $statement2->error));
+    exit;
 }
 
 // Close the statements
 $statement1->close();
 $statement2->close();
+
+// Success message
+header('Location: registerform.php?success=' . urlencode('User registered successfully!'));
+exit;
 ?>
